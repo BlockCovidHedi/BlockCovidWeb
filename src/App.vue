10ad06964acc7ee528927eb4f435403b3a32f5cf
Hedi Mzoughi <hmz@niboo.com>
2021-08-21 22:56:19 +0200

    change nav view

--- src/App.vue
+++ src/App.vue
@@ -1,42 +1,53 @@
 <template>
   <div>
     <header>
-        <b-navbar toggleable="lg" type="dark" variant="dark" id="nav">
-          <b-navbar-brand>BLOCKCOVID</b-navbar-brand>
-          <div v-if="token">
-            <router-link to="/connected">Home</router-link>
-            <a href="#" @click.prevent="logout()">Se déconnecter</a>
-          </div>
-
-          <div v-else>
-            <router-link to="/">Home</router-link>
-            <router-link to="/about">A propos</router-link>
-          </div>
-        </b-navbar>
+      <b-navbar id="nav" toggleable="sm" type="dark" variant="dark">
+        <b-navbar-brand>BLOCKCOVID</b-navbar-brand>
+        <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+        <b-collapse id="nav-collapse" is-nav>
+          <template v-if="token">
+            <b-navbar-nav>
+              <b-nav-item to="/connected">Home</b-nav-item>
+            </b-navbar-nav>
+            <b-navbar-nav class="ml-auto">
+              <b-nav-item-dropdown text="Paramètre" right>
+                <b-dropdown-item to="/">Profile</b-dropdown-item>
+                <b-dropdown-item href="/" @click.prevent="logout()">Se déconnecter</b-dropdown-item>
+              </b-nav-item-dropdown>
+            </b-navbar-nav>
+          </template>
+          <template v-else>
+            <b-navbar-nav>
+              <b-nav-item to="/">Home</b-nav-item>
+              <b-nav-item to="/about">A propos</b-nav-item>
+            </b-navbar-nav>
+          </template>
+        </b-collapse>
+      </b-navbar>
     </header>
     <body class="mt-5">
-
-      <router-view/>
+    <router-view/>
     </body>
     <footer>
-      <img src="./assets/covid_logo_footer.jpg" height="100" width="250" alt="covid logo">
+      <img alt="covid logo" height="100" src="./assets/covid_logo_footer.jpg" width="250">
     </footer>
   </div>
 </template>
 <script>
-  import Users from './services/users'
-  import Route from './router/index'
-  export default {
-    data : () => ({
-      token: Users.isToken()
-    }),
-    methods : {
-      logout : function() {
-        Users.logout()
-        Route.push('/')
-      }
+import Users from './services/users'
+import Route from './router/index'
+
+export default {
+  data: () => ({
+    token: Users.isToken()
+  }),
+  methods: {
+    logout: function () {
+      Users.logout()
+      Route.push('/')
     }
   }
+}
 
 </script>
 
@@ -50,18 +61,15 @@
 }
 
 #nav a {
-  padding: 50px;
-}
-
-#nav a {
   font-weight: bold;
 }
 
 #nav a.router-link-exact-active {
   color: #42b983;
 }
+
 footer {
-  position:fixed;
+  position: fixed;
   bottom: 0;
   right: 0;
 }
